35e7b6bf95f45090ebb59ab672a58b6600add0ea
D. Lisovin <d.lisovin.dev@gmail.com>
2025-08-04 20:21:46 +0300

    Added DB support

--- components/calculator/EvenSumCalculator.php
+++ components/calculator/EvenSumCalculator.php
@@ -3,6 +3,7 @@
 namespace app\components\calculator;
 
 use app\components\calculator\CalculatorInterface;
+use app\models\dto\IntegerArrayDto;
 
 class EvenSumCalculator implements CalculatorInterface
 {
@@ -19,5 +20,85 @@ class EvenSumCalculator implements CalculatorInterface
             }
         }
         return $sum;
+    }
+
+    public function maxByFlip_9_6(int $number)
+    {
+        $numString = (string)$number;
+        $flipPosition = strpos($numString, '6');
+        if ($flipPosition === false) {
+            return $number;
+        }
+        $numString[$flipPosition] = '9';
+        return (int)$numString;
+    }
+
+    function estimate0(array $levels): int
+    {
+        $totalSum = 0;
+        $arrLength = count($levels);
+
+        for ($i = 0; $i < $arrLength - 1; $i++) {
+            if ($levels[$i + 1] < $levels[$i]) {// found a potential open pool
+                $poolLevel = $levels[$i];
+                $poolSum = 0;
+
+                for ($j = $i + 1; $j < $arrLength; $j++) {
+                    $depth = $poolLevel - $levels[$j];
+                    if ($depth <= 0) { // the pool is closed
+                        $totalSum += $poolSum;
+                        $i = $j;
+                        break;
+                    } else {
+                        $poolSum += $depth;
+                        if ($j == $arrLength - 1) { //"drain" unclosed pool on the right
+                            $poolSum -= ($j - $i) * ($poolLevel - $levels[$j]);
+                            $totalSum += $poolSum;
+                            break;
+                        }
+                    }
+                }
+            } else {
+                $i++;
+            }
+        }
+
+        return $totalSum;
+    }
+
+    function estimate(array $levels)
+    {
+        $totalSum = 0;
+        $uniquelevels = array_unique($levels);
+        sort($uniquelevels);
+
+        $left = 0;
+        $right = count($levels)-1;
+        for ($i = 1; $i < count($uniquelevels); $i++) {
+            $depth = $uniquelevels[$i] - $uniquelevels[$i-1];
+            $newLeft = $left;
+            $newRight = $right;
+            for ($j = $left; $j <=$right; $j++) {
+                if ($levels[$j] >= $uniquelevels[$i] ){
+                    $newLeft = $j;
+                    break;
+                }
+            }
+            for ($k = $right; $k >= $left; $k--) {
+                if ($levels[$k] >= $uniquelevels[$i] ){
+                    $newRight = $k;
+                    break;
+                }
+            }
+            for ($w = $newLeft; $w <= $newRight; $w++) {
+                if ($levels[$w] < $uniquelevels[$i] ){
+                   $totalSum +=$depth;
+                }
+            }
+            $left = $newLeft;
+            $right = $newRight;
+
+        }
+        return $totalSum;
     }
 }
--- tests/unit/calculators/EvenSumCalculatorTest.php
+++ tests/unit/calculators/EvenSumCalculatorTest.php
@@ -50,12 +50,30 @@ final class EvenSumCalculatorTest extends TestCase
         $this->assertEquals(4, $calculator->calculate($args));
     }
 
+    public function testflip69()
+    {
 
+        $calculator = new EvenSumCalculator();
+        $this->assertEquals(9666, $calculator->maxByFlip_9_6(6666));
+        $this->assertEquals(9999, $calculator->maxByFlip_9_6(9999));
+        $this->assertEquals(9999, $calculator->maxByFlip_9_6(6999));
+        $this->assertEquals(9969, $calculator->maxByFlip_9_6(9669));
+    }
 
+    public function testWhatever(){
+        $calculator = new EvenSumCalculator();
+        $this->assertEquals(2,$calculator->estimate([33, 33, 5, 7]));
+        $this->assertEquals(6,$calculator->estimate([1,0,2,1,0,1,3,2,1,2,1]));
+        $this->assertEquals(7,$calculator->estimate([3,0,2,0,4]));
+        $this->assertEquals(4,$calculator->estimate([2,0,0,2]));
+        $this->assertEquals(1,$calculator->estimate([1,0,1]));
+        assert($calculator->estimate([33, 33, 5, 7]) == 2);
+        assert($calculator->estimate([1,0,2,1,0,1,3,2,1,2,1]) == 6);
+        assert($calculator->estimate([3,0,2,0,4]) == 7);
+        assert($calculator->estimate([2,0,0,2]) == 4);
+        assert($calculator->estimate([1,0,1]) == 1);
+        assert($calculator->estimate([99,99,99]) == 0);
 
-
-
-
-
+    }
 
 }
